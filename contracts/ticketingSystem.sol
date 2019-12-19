f868ec6fb1ee9386d6b991eb6cbd0fc67b83f984
HADHRI <hadhri.anas@gmail.com>
2019-12-19 22:04:46 +0100

    Create and modifying Venue Profile

--- contracts/ticketingSystem.sol
+++ contracts/ticketingSystem.sol
@@ -4,7 +4,9 @@ contract ticketingSystem  {
 
 
 mapping (uint => Artist) public artistsRegister;  
+mapping (uint => Venue) public venuesRegister;  
 uint numbersOfArtists = 0; 
+uint numberOfVenue=0;
 
 event ArtistCreated(uint artistCategory, string artistName,address artistAddress);
 
@@ -16,6 +18,13 @@ struct Artist {
         address owner; 
 }
 
+struct Venue {
+        bytes32 name;
+        uint capacity;
+        uint standardComission;
+        address owner;
+}
+
 
 // Function to create  an Artist
 function createArtist(string memory name,uint _artistCategory) public {
@@ -24,11 +33,27 @@ numbersOfArtists++;
 artistsRegister[numbersOfArtists]=artist; 
 }
 //Function to modify the Artist Profile 
-function modifyArtist(uint _artistId, string memory _name, uint _artistCategory,address owner) public{
+function modifyArtist(uint _artistId, string memory _name, uint _artistCategory,address payable owner) public{
 require(artistsRegister[_artistId].owner == msg.sender);
 artistsRegister[_artistId].artistCategory=_artistCategory;
 artistsRegister[_artistId].name= _name;
 artistsRegister[_artistId].owner= owner;
+}
+
+//Create venue Profile 
+function createVenue(bytes32 _name, uint _capacity, uint _standardComission) public {
+        Venue memory venue = Venue(_name,_capacity,_standardComission,msg.sender);
+        numberOfVenue ++;
+        venuesRegister[numberOfVenue]=venue;  
+}
+
+//Modify Venue Profile 
+function modifyVenue(uint _venueId, bytes32 _name, uint _capacity, uint _standardComission, address payable _newOwner) public{
+        require(venuesRegister[_venueId].owner == msg.sender);
+        venuesRegister[_venueId].name=_name;
+        venuesRegister[_venueId].capacity=_capacity;
+        venuesRegister[_venueId].standardComission=_standardComission;
+        venuesRegister[_venueId].owner=_newOwner;
 }
 
 
